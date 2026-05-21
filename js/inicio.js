1f09a526764908d027df3dc80c89b9b6e3f50900
danzxw7 <danielleite0308@hotmail.com>
2026-05-21 19:00:43 -0300

    base pronta

---
+++ js/inicio.js
@@ -0,0 +1,30 @@
+// MENU
+
+function toggleMenu() {
+    document.getElementById("sidebar").classList.toggle("active");
+}
+
+// CARROSSEL
+
+let slideIndex = 0;
+
+showSlides();
+
+function showSlides() {
+
+    let slides = document.getElementsByClassName("slide");
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slideIndex++;
+
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+
+    setTimeout(showSlides, 4000);
+}
